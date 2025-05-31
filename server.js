dffe26e970877be854458438b963cab651408a77
Aman Kushwaha <amankushwaha7284@gmail.com>
2025-05-31 18:43:40 +0530

    Updated server port

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose.connect(db,{
 });
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 })
